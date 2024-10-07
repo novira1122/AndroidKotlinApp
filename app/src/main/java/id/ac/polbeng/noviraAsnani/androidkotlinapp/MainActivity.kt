fc5ce2c2ee3ef0c1efb0c8c0a2320fbf6f432be8
Modern 14 <noviraasnani506@gmail.com>
2024-10-07 19:57:57 +0700

    first commit.

---
+++ app/src/main/java/id/ac/polbeng/noviraAsnani/androidkotlinapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package id.ac.polbeng.noviraAsnani.androidkotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import id.ac.polbeng.noviraAsnani.androidkotlinapp.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvCounter.text = "1"
+    }
+    fun addNumber(v: View){
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        binding.tvCounter.text = nextVal.toString()
+    }
+    fun resetNumber(v: View){
+        binding.tvCounter.text = "0"
+    }
+    fun subtractNumber(v: View){
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
+        binding.tvCounter.text = nextVal.toString()
+    }
+}
